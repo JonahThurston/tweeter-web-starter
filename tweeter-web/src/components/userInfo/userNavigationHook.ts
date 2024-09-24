c19e1c4b1c6eaa4a3b1b63d1495ff404d8245598
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-09-24 16:32:24 -0600

    Add userInfo Hook (Milestone 1 done!!)

--- tweeter-web/src/components/userInfo/userNavigationHook.ts
+++ tweeter-web/src/components/userInfo/userNavigationHook.ts
@@ -1,7 +1,6 @@
-import { useContext } from "react";
 import { AuthToken, FakeData, User } from "tweeter-shared";
-import { UserInfoContext } from "./UserInfoProvider";
 import useToastListener from "../toaster/ToastListenerHook";
+import useUserInfo from "./userInfoHook";
 
 interface UserNavigation{
     navigateToUser: (event: React.MouseEvent) => Promise<void>,
@@ -10,7 +9,7 @@ interface UserNavigation{
 const useUserNavigation = (): UserNavigation => {
     const { displayErrorMessage } = useToastListener();
 
-    const { setDisplayedUser, currentUser, authToken } = useContext(UserInfoContext);
+    const { setDisplayedUser, currentUser, authToken } = useUserInfo();
 
     const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
         event.preventDefault();
