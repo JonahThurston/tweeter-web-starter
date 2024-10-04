d36216be8e7f0069caa3f97b4424af26cb6c36c4
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-10-04 14:20:10 -0600

    UserNavigation mvp'd

--- tweeter-web/src/components/userInfo/userNavigationHook.ts
+++ tweeter-web/src/components/userInfo/userNavigationHook.ts
@@ -1,52 +1,34 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "./userInfoHook";
+import {
+  UserNavigationPresenter,
+  UserNavigationView,
+} from "../../presenters/UserNavigationPresenter";
+import { useState } from "react";
 
-interface UserNavigation{
-    navigateToUser: (event: React.MouseEvent) => Promise<void>,
+interface UserNavigation {
+  navigateToUser: (event: React.MouseEvent) => Promise<void>;
 }
 
 const useUserNavigation = (): UserNavigation => {
-    const { displayErrorMessage } = useToastListener();
+  const { displayErrorMessage } = useToastListener();
 
-    const { setDisplayedUser, currentUser, authToken } = useUserInfo();
+  const { setDisplayedUser, currentUser, authToken } = useUserInfo();
 
-    const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-        event.preventDefault();
-    
-        try {
-          const alias = extractAlias(event.target.toString());
-    
-          const user = await getUser(authToken!, alias);
-    
-          if (!!user) {
-            if (currentUser!.equals(user)) {
-              setDisplayedUser(currentUser!);
-            } else {
-              setDisplayedUser(user);
-            }
-          }
-        } catch (error) {
-          displayErrorMessage(`Failed to get user because of exception: ${error}`);
-        }
-    };
-    
-    const extractAlias = (value: string): string => {
-        const index = value.indexOf("@");
-        return value.substring(index);
-    };
-    
-    const getUser = async (
-            authToken: AuthToken,
-            alias: string
-        ): Promise<User | null> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.findUserByAlias(alias);
-    };
+  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
+    presenter.navigateToUser(event, authToken!, currentUser!);
+  };
 
-    return {
-        navigateToUser: navigateToUser
-    }
-}
+  const listener: UserNavigationView = {
+    displayErrorMessage: displayErrorMessage,
+    setDisplayedUser: setDisplayedUser,
+  };
+
+  const [presenter] = useState(new UserNavigationPresenter(listener));
+
+  return {
+    navigateToUser: navigateToUser,
+  };
+};
 
 export default useUserNavigation;
